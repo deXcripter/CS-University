03aead8f2f6a862578ac7fffeab9dcd58526277c
Johnpaul <indisputable.jay@gmail.com>
2024-02-02 15:56:46 +0100

    testing the ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
+
+app.use('/', (req, res) => {
+  res.send('<h1> Hello from the server </h1>');
+});
 
 export default app;
